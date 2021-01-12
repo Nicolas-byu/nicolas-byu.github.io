66592360ba57e74e1a602b09260c7da39023acf4
nicolas-p-orellana <ore19006@byui.edu>
2021-01-12 16:15:48 -0300

    Add Current Year

--- js/script.js
+++ js/script.js
@@ -1,2 +1,4 @@
+CurrentYear = new Date().getFullYear();
+document.getElementById("current-year").innerHTML= CurrentYear;
 var lastModified = document.lastModified;
 document.getElementById("updated").innerHTML = lastModified;
