83224a50fe47ce53461cea5d5989d341137045f2
nicolas-p-orellana <ore19006@byui.edu>
2021-02-27 10:55:19 -0300

    Finished Lazy Loading assingment

--- Lesson7/js/script.js
+++ Lesson7/js/script.js
@@ -1,53 +1,36 @@
-var date = new Date();
-let currentDate = date.getDate();
-let currentMonth = Number(date.getMonth()) + 1;
-let currentYear = date.getFullYear();
-let i = 0;
-
-// Sets the days for the Five Day Forecast Table.
-while (i < 5) {
-  date.setDate(date.getDate() + i);
-  let currentDay = date.toLocaleString("default", { weekday: "short" });
-  date.setDate(date.getDate() - i);
-  document.getElementById(`day${i + 1}`).innerHTML = currentDay;
-  i++;
-}
-
-// Adds a 0 before the days and months if they are smaller than 10.
-if (currentDate < 10) {
-  currentDate = "0" + currentDate;
-}
-if (currentMonth < 10) {
-  currentMonth = "0" + currentMonth;
-}
-
-document.getElementById("current-date").innerHTML =
-  currentDate + "/" + currentMonth + "/" + currentYear;
-
-//Sets the hamburguer menu.
-const hamButton = document.querySelector(".menu");
-const menuUl = document.querySelector(".navigation");
-
-hamButton.addEventListener(
-  "click",
-  () => {
-    menuUl.classList.toggle("responsive");
-  },
-  false
-);
-
-//Shows a message when is Saturday
-const element = document.getElementById("message");
-
-if (date.getDay() === 6) {
-  element.classList.add("showme");
-} else {
-  element.classList.add("hideme");
-}
-// Ensures that Poppins loads properly.
-
 WebFont.load({
   google: {
     families: ["Poppins"],
   },
 });
+
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const imgOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px 50px 0px",
+};
+
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+if ("IntersectionObserver" in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
